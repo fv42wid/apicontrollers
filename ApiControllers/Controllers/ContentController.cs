78a3742128557f99d2d925ef0365029616dbeec5
Frank Vinci <fv42wid@yahoo.com>
2017-04-08 16:55:47 +0000

    finish chapter 20

---
+++ ApiControllers/Controllers/ContentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiControllers.Models;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller
+    {
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object/{format?}")]
+        [FormatFilter]
+        //[Produces("application/json", "application/xml")]
+        //[Produces("application/xml")]
+        public Reservation GetObject() => new Reservation
+        {
+            ReservationId = 100,
+            ClientName = "Joe",
+            Location = "Board Room"
+        };
+
+        [HttpPost("poster")]
+        [Consumes("application/json")]
+        public Reservation ReceiveJson([FromBody] Reservation reservation)
+        {
+            reservation.ClientName = "Json";
+            return reservation;
+        }
+
+        [HttpPost]
+        [Consumes("application/xml")]
+        public Reservation ReceiveXml([FromBody] Reservation reservation)
+        {
+            reservation.ClientName = "Xml";
+            return reservation;
+        }
+    }
+}
